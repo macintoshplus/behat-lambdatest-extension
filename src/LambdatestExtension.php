3e542b873cfd55176087160e9d44a566b85a5cf9
jb cr <51637606+jbcr@users.noreply.github.com>
2021-06-04 11:59:48 +0200

    add session listener to set state on lambdatest

--- src/LambdatestExtension.php
+++ src/LambdatestExtension.php
@@ -3,10 +3,13 @@
 namespace Macintoshplus\Lambdatest;
 
 use Behat\MinkExtension\ServiceContainer\MinkExtension;
+use Behat\Testwork\EventDispatcher\ServiceContainer\EventDispatcherExtension;
 use Behat\Testwork\ServiceContainer\Extension as ExtensionInterface;
 use Behat\Testwork\ServiceContainer\ExtensionManager;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
 class LambdatestExtension implements ExtensionInterface
 {
@@ -41,6 +44,14 @@ class LambdatestExtension implements ExtensionInterface
      */
     public function load(ContainerBuilder $container, array $config)
     {
+        //SessionStateListener
+        $definition = new Definition('Macintoshplus\Lambdatest\Listener\SessionStateListener', [
+            new Reference(MinkExtension::MINK_ID),
+            '%mink.javascript_session%',
+            '%mink.available_javascript_sessions%',
+        ]);
+        $definition->addTag(EventDispatcherExtension::SUBSCRIBER_TAG, ['priority' => 0]);
+        $container->setDefinition('mink.lambdatest.listener.sessions', $definition);
     }
 
     /**
